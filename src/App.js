a0ff95ae389a97e0d1a23076d0c1fbf66b1c4594
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-06-21 20:26:38 +0100

    Added conversion inputs to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Title from './title';
 import Conversiontable from './conversiontable';
+import Conversioninput from './conversioninput';
 
 function App() {
   return (
     <div className="App">
     <Title />
+    <Conversioninput />
     <Conversiontable />
     </div>
   );
---
+++ src/conversioninput.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Conversioninput()  {
+    return  (
+    <div>
+        <label id="copper">Copper</label>
+        <input type="number" id="cp"></input>
+
+        <label id="silver">Silver</label>
+        <input type="number" id="sp"></input>
+
+        <lable id="electrum">Electrum</lable>
+        <input type="number" id="ep"></input>
+        
+        <label id="gold">Gold</label>
+        <input type="number" id="gp"></input>
+
+        <label id="plat">Platinum</label>
+        <input type="number" id="pp"></input>
+
+        <label id="party">Party Size</label>
+        <input type="number" id="partysize"></input>
+    </div>
+    )
+}
+
+export default Conversioninput
